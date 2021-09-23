286b92f42bc44ee69c1aa879647710bced49232d
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-23 01:42:42 -0300

    :zap: feat: Added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyle } from './styles/global'
+
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello world</h1>
+    </>
+  )
 }
 
 export default App
