1a45c5e1d8b2f99f56dbfb5029a63bed595fc1d9
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-23 17:14:46 -0300

    :wrench: feat: Adding routes for the other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { Home } from './components/Home'
+import { NavBar } from './components/NavigationBar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import { GlobalStyle } from './styles/global'
+import { About } from './components/About'
+import { Projects } from './components/Projects'
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
-      <h1>Hello world</h1>
-    </>
+      <NavBar />
+      <Switch>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
