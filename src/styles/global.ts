015fc9bf3b8402d961ab6225b771852ae1c5d9b0
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-23 01:41:33 -0300

    :art: styles: Choosing a color pallete for the project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,33 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --black-color: #393E46;
+    --white-color: #f0f0e4;
+
+    --primary-color: #DB5F29;
+    --secondary-color: #68BDE1;
+    --alternative-color: #7B3C3C;
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    min-height: 100vh;
+    background-color: #393E46;
   }
 `
