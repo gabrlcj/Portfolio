d28e8f8f199ea1fbb2f3e722623568422ff08928
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-24 02:24:34 -0300

    :tada: feat: Added function to make the typing effect

---
+++ src/utils/useTypedText.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+
+export function useTypedText(text: string, speed: number, delayTime?: number) {
+  const [textState, setTextState] = useState('')
+  const chars = text.split('')
+  let interval = speed
+
+  if (textState.length === 0 && delayTime) interval = speed + delayTime
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTextState((prevText) => {
+        if (prevText.length !== chars.length) {
+          const newText = prevText.concat(chars[prevText.length])
+          return newText
+        }
+        return prevText
+      })
+    }, interval)
+    return () => clearTimeout(timer)
+  })
+
+  return textState
+}
