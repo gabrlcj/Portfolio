46b65b43f78a833927e4348bd6e0b2f5534284a5
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-25 01:39:47 -0300

    :art: feat: Started to write the content of About component

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,16 +1,114 @@
-import { Container } from './styles'
+import { ApresentationBox, Container } from './styles'
+import Ilustration from '../../assets/Image-about.svg'
+import { useTypedText } from '../../utils/useTypedText'
+import {
+  SiCss3,
+  SiGit,
+  SiHtml5,
+  SiJavascript,
+  SiNextDotJs,
+  SiNpm,
+  SiReact,
+  SiReactrouter,
+  SiSass,
+  SiStyledComponents,
+  SiTypescript,
+  SiYarn,
+} from 'react-icons/si'
 
 export function About() {
   return (
     <Container>
-      <h1>About me</h1>
-      <div>
-        <h2>
+      <h1>{useTypedText('About me', 75, 30)}</h1>
+      <ApresentationBox>
+        <h2>{useTypedText('Biography', 80, 35)}</h2>
+        <h3>
           Hi people, this is a section more about me I started studiyng
-          front-end programming this year with HTML and CSS. From there I have
-          go to Javascript and here we are now 😁.
+          front-end programming this year with HTML and CSS, from there on I
+          went to Javascript, and when I got more confident with the language I
+          began with React.js 😁.
+        </h3>
+        <section>
+          <img src={Ilustration} alt="Ilustration of man playing video games" />
+          <div className="bio-container">
+            <h3>
+              I live in Paraná, Brasil and I'm pursuing my first job as a
+              front-end programmer, very excited to have this experience. I'm
+              currently on career transition.
+            </h3>
+            <h3>
+              I have some experience working with a team from a hackathon that I
+              participate and individual experience with some projects that I
+              use to practice some of the languages I'm trying to know and work
+              with.
+            </h3>
+            <h3>
+              Some of my hobbies are play video games with my friends 🎮, hang
+              out with my friends 🎉, watch NBA 🏀 and do exercises 🏃🏽‍♂️.
+            </h3>
+          </div>
+        </section>
+      </ApresentationBox>
+      <br />
+      <ApresentationBox>
+        <h2>
+          {useTypedText(
+            "Languages and tools I'm learning & I'm working with:",
+            80,
+            35
+          )}
         </h2>
-      </div>
+        <div className="icons-container">
+          <div>
+            <SiHtml5 style={{ color: '#e34f26' }} />
+            <strong>HTML5</strong>
+          </div>
+          <div>
+            <SiCss3 style={{ color: '#1572B6' }} />
+            <strong>CSS3</strong>
+          </div>
+          <div>
+            <SiSass style={{ color: '#CC6699' }} />
+            <strong>SASS</strong>
+          </div>
+          <div>
+            <SiJavascript style={{ color: '#F7DF1E' }} />
+            <strong>JAVASCRIPT</strong>
+          </div>
+          <div>
+            <SiGit style={{ color: '#F05032' }} />
+            <strong>GIT</strong>
+          </div>
+          <div>
+            <SiNpm style={{ color: '#CB3837' }} />
+            <strong>NPM</strong>
+          </div>
+          <div>
+            <SiYarn style={{ color: '#2C8EBB' }} />
+            <strong>YARN</strong>
+          </div>
+          <div>
+            <SiReact style={{ color: '#61DAFB' }} />
+            <strong>REACT.JS</strong>
+          </div>
+          <div>
+            <SiReactrouter style={{ color: '#CA4245' }} />
+            <strong>REACT ROUTER</strong>
+          </div>
+          <div>
+            <SiStyledComponents style={{ color: '#DB7093' }} />
+            <strong>STYLED-COMPONENTS</strong>
+          </div>
+          <div>
+            <SiNextDotJs style={{ color: '#000000' }} />
+            <strong>NEXT.JS</strong>
+          </div>
+          <div>
+            <SiTypescript style={{ color: '#3178C6' }} />
+            <strong>TYPESCRIPT</strong>
+          </div>
+        </div>
+      </ApresentationBox>
     </Container>
   )
 }
--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -1,13 +1,9 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-
   height: 100vh;
   padding: 2rem;
+  line-height: 2.5rem;
   animation: fadeIn 2s forwards;
 
   @keyframes fadeIn {
@@ -21,5 +17,98 @@ export const Container = styled.main`
   }
 
   h1 {
+    font-size: 4rem;
+    color: var(--secondary-color);
+    margin: 2rem 0 3rem 3rem;
+    text-shadow: 0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.5);
+  }
+
+  h3 {
+    color: var(--white-color);
+  }
+
+  h2 {
+    color: var(--secondary-color);
+    margin-bottom: 1rem;
+    text-shadow: 0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.5);
+  }
+`
+
+export const ApresentationBox = styled.div`
+  text-overflow: clip;
+  padding: 2rem;
+  background: rgba(0, 0, 0, 0.15);
+  border-radius: 1rem;
+
+  section {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    gap: 2.5rem;
+
+    img {
+      margin-top: 1rem;
+      width: 25rem;
+      animation: float2 3s infinite;
+
+      @keyframes float2 {
+        0% {
+          transform: translate(0, 0.5rem);
+        }
+        50% {
+          transform: translate(0.5rem, 0);
+        }
+        100% {
+          transform: translate(0, 0.5rem);
+        }
+      }
+    }
+
+    .bio-container {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      flex-direction: column;
+      gap: 3rem;
+    }
+  }
+
+  .icons-container {
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    justify-content: center;
+    gap: 1rem;
+    grid-template-columns: repeat(4, 20%);
+    grid-template-rows: 10rem 10rem 10rem;
+    position: relative;
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+
+      svg {
+        color: var(--white-color);
+        width: 5rem;
+        height: 5rem;
+
+        transition: transform ease 0.3s;
+
+        &:hover {
+          transform: translateY(-25%);
+        }
+      }
+
+      strong {
+        margin-top: 0.75rem;
+        color: var(--white-color);
+        background: rgba(0, 0, 0, 0.1);
+        border-radius: 0.5rem;
+        padding: 0 0.75rem;
+        text-shadow: 0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.5);
+      }
+    }
   }
 `
