7e5d9b25dbc7d7ccfe3bec94098f9ec8c343a49e
Gabriel <gabrielbittencourt57@gmail.com>
2021-09-29 01:13:30 -0300

    :sparkles: feat: Created footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+
+export function Footer() {
+  return (
+    <Container>
+      <h4>Made with 🤎 by Gabriel Bittencourt</h4>
+    </Container>
+  )
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.footer`
+  background: rgba(0, 0, 0, 0.15);
+  border-radius: 1rem;
+  padding: 0.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h4 {
+    color: var(--secondary-color);
+    font-size: 1rem;
+  }
+`
