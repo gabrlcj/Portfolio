1f410cd5a3436302412eee113cdb7fa7ebec4798
Gabriel <gabrielbittencourt57@gmail.com>
2021-10-26 22:56:35 -0300

    :art: chore: Made changes on the structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { NavBar } from './components/NavigationBar'
-import { Home } from './components/Home'
-import { About } from './components/About'
-import { Projects } from './components/Projects'
 import { ButtonGoToTop } from './components/ButtonGoToTop'
-
 import { GlobalStyle } from './styles/global'
+import { Routes } from './routes'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <NavBar />
-      <Switch>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes />
       <ButtonGoToTop />
     </Router>
   )
